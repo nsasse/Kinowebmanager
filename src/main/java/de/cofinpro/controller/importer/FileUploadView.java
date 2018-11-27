eba9a5f622bb0844ff3e04edfa89726c8aea633f
nico <nico.sasse@cofinpro.de>
2018-11-27 14:14:25 +0100

    FileUpload Multiple

--- src/main/java/de/cofinpro/controller/importer/FileUploadView.java
+++ src/main/java/de/cofinpro/controller/importer/FileUploadView.java
@@ -14,34 +14,57 @@ import de.cofinpro.controller.XmlAusgabe;
 
 @ManagedBean(name = "fileUploadView")
 public class FileUploadView {
-	     
-	    private UploadedFile file;
-	 
-	    public UploadedFile getFile() {
-	        return file;
-	    }
-	 
-	    public void setFile(UploadedFile file) {
-	        this.file = file;
-	    }
-	     
-	    public void upload() {
-	        if(file != null) {
-	            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
-	            FacesContext.getCurrentInstance().addMessage(null, message);
-	        }
-	    }
-	     
-	    public void handleFileUpload(FileUploadEvent event) throws InvalidFormatException, IOException {
-	        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
-	        FacesContext.getCurrentInstance().addMessage(null, msg);
-	        
-	    	XmlAusgabe ausgabe = new XmlAusgabe();
-	    	
-	    	ausgabe.start(event.getFile());
-	    	
-	    	System.out.println("XML wurde erstellt!");
-	    	
-	        
-	    }
+
+	private UploadedFile file;
+
+	public UploadedFile getFile() {
+		return file;
 	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
+	public void upload() {
+		if (file != null) {
+			FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
+
+	int counterFile = 0;
+	UploadedFile filme;
+	UploadedFile saele;
+	UploadedFile werbespots;
+	
+
+	public void handleFileUpload(FileUploadEvent event) throws InvalidFormatException, IOException {
+		FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+
+		System.out.println(event.getFile().getFileName());
+		counterFile++;
+		String name = new String();
+		name = event.getFile().getFileName();
+
+		if (name == "filme.csv") {
+			filme = event.getFile();
+		}
+		
+		if (name == "saele.csv") {
+			saele = event.getFile();
+		}
+		
+		if (name == "werbespots.csv") {
+			werbespots = event.getFile();
+		}
+
+		if (counterFile == 3) {
+			
+			XmlAusgabe ausgabe = new XmlAusgabe();
+
+			ausgabe.start(filme, saele, werbespots);
+		}
+
+	}
+}
